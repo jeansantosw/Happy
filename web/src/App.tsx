96207735057b317b4bbab185247e87e54bda5644
Jean Santos <jeansantos.sw@gmail.com>
2021-02-26 18:11:15 +0000

    Atualizando

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,8 @@ import './styles/global.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
+      <p>Transforming</p>
     </div>
   );
 }
