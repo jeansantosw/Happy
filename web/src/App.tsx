39855464f05591ac432249bbc284491d7a6fdd39
Jean Santos <jeansantos.sw@gmail.com>
2021-02-26 23:07:28 +0000

    new

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+
+
 import './styles/global.css';
+import './styles/pages/landing.css';
+
+
+import logoImg from './images/logo.svg';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <p>Transforming</p>
+    <div id="page-landing">
+     <div className="content-wrapper">
+       <img src={logoImg} alt="Happy"/>
+
+       <main>
+         <h1>Leve felicidade para o mundo</h1>
+         <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+       </main>
+
+       <div className="location">
+         <strong>Amazonas</strong>
+         <span>Manaus</span>
+       </div>
+
+       <a href="" className="enter-app"> ENTRAR </a>
+
+     </div>
     </div>
   );
 }
